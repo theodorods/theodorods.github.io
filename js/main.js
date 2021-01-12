074e31fa1ae0f3727292ca0b7074359b72655386
Bjørn Theodor Odsbu <bjorntheodor.odsbu@gmail.com>
2021-01-12 15:27:54 +0100

    Added non-functional navbar.

--- js/main.js
+++ js/main.js
@@ -67,6 +67,25 @@
 }
 
     /*==================================================================
+    [ Navbar ]*/
+    window.onscroll = function() {myFunction()};
+
+    // Get the navbar
+    var navbar = document.getElementById("navbar")
+
+    // Get the offset position of the navbar
+    var sticky = navbar.offsetTop;
+
+    // Add the sticky class to the navbar when you reach its scroll position. Remove "sticky" when you leave the scroll position
+    function myFunction() {
+        if (window.pageYOffset >= sticky) {
+            navbar.classList.add("sticky")
+        } else {
+            navbar.classList.remove("sticky")
+        }
+    }
+
+    /*==================================================================
     [ Modal ]*/
     $('.modal-subscribe').on('click',function(e){
         e.stopPropagation();
